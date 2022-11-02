40012ea9c8a855009045fffcafd415ca62395720
Leuluch-12037716 <juan32choque@gmail.com>
2022-11-02 15:55:23 +0100

    Update dinosaur.py

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,16 +1,21 @@
-from utils.constants import RUNNING
+import pygame
+
+from urllib.parse import SplitResult
+
+from dino_runner.utils.constants import RUNNING
 
 from pygame.sprite import Sprite
+
 class Dinosaur(Sprite):
-        def __int__(self):
-            self.image = RUNNING[0]
-            self.image = self.image.get_part()
-            self.image_rect.x = 50
-            self.image_rect.y = 300
+    def __init__(self):
+        self.image = RUNNING[0]
+        self.image_rect = self.image.get_rect()
+        self.image_rect.x = 50
+        self.image_rect.y = 300
 
-        def update(self):
-            print("Updating")
+    def update(self):
+        print("Updating")
 
-        def draw(self, screen):
-            screen.blit(self.image, (self.image_rect.x, self.image_rect.y))
+    def draw(self, screen):
+        screen.blit(self.image, (50, 300))
             
