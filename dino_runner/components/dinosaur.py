5d801510b994b3a9f2aaa692db4ebd319cc1e5e0
Leuluch-12037716 <juan32choque@gmail.com>
2022-11-03 09:39:00 +0100

    Update dinosaur.py

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -2,26 +2,67 @@ import pygame
 
 from urllib.parse import SplitResult
 
-from dino_runner.utils.constants import RUNNING
+from dino_runner.utils.constants import RUNNING, JUMPING
 
 from pygame.sprite import Sprite
 
+
+
 class Dinosaur(Sprite):
+    Dino_x_pos = 50
+    Dino_y_pos = 300
+    Initial_step = 0
+    Max_step = 10
+    Aceleration = 4
+    Reduce_velocity = 1
+    Initial_velocity = 10
+    
     def __init__(self):
         self.image = RUNNING[0]
         self.image_rect = self.image.get_rect()
-        self.image_rect.x = 50
-        self.image_rect.y = 300
-        self.step = 0 
+        self.image_rect.x = self.Dino_x_pos
+        self.image_rect.y = self.Dino_y_pos
+        self.step = self.Initial_step 
+        self.dino_jump = False
+        self.dino_run = True
+        self.dino_velocity = self.Initial_velocity
 
-    def update(self):
+
+    def update(self, dino_event):
+        if self.dino_jump:
+            self.jump()
+        if self.dino_run:
+            self.run()
+
+        if dino_event[pygame.K_SPACE] and not self.dino_jump:
+            self.dino_run = False
+            self.dino_jump = True
+        #else:
+         #   self.dino_run = True
+          #  self.dino_jump = False
+            
+
+        if self.step > self.Max_step:    
+            self.step = self.Initial_step
+
+    def run(self):
         self.image = RUNNING[0] if self.step <= 5 else RUNNING[1]
         self.image_rect = self.image.get_rect()
-        self.image_rect.x = 50
-        self.image_rect.y = 300
+        self.image_rect.x = self.Dino_x_pos
+        self.image_rect.y = self.Dino_y_pos
         self.step += 1
-        if self.step > 10:    
-            self.step = 0
+        
+
+    def jump(self):
+        self.image = JUMPING
+        if self.dino_jump:
+            self.image_rect.y -= self.dino_velocity * self.Aceleration
+            self.dino_velocity -= self.Reduce_velocity
+        if self.dino_velocity < -self.Initial_velocity:
+            self.image_rect.y = self.Dino_y_pos
+            self.dino_jump = False
+            self.dino_velocity = self.Initial_velocity
+            self.dino_run = True
 
     def draw(self, screen):
         screen.blit(self.image, (self.image_rect.x, self.image_rect.y))
