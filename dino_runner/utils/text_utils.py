75c1e476726de5ddaba81baccb43d924098534d9
Leuluch-12037716 <juan32choque@gmail.com>
2022-11-03 23:42:54 +0100

    Create text_utils.py

---
+++ dino_runner/utils/text_utils.py
@@ -0,0 +1,14 @@
+from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+import pygame
+
+FONT_STYLE = "freesansbold.ttf"
+BLACK_RGB = (0, 0, 0)
+
+def get_text_element(messege, pos_x = SCREEN_WIDTH //2, pos_y = SCREEN_HEIGHT //2, font_size = 30):
+    font = pygame.font.Font(FONT_STYLE, font_size)
+
+    text = font.render(messege, True, BLACK_RGB)
+    text_rect = text.get_rect()
+    text_rect.center = (pos_x, pos_y)
+
+    return text, text_rect
