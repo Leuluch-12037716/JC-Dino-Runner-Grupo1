54bb9865ae458d5cf4922cf8a8008d86c921932b
Leuluch-12037716 <juan32choque@gmail.com>
2022-11-04 13:06:41 +0100

    Update obstacle.py

--- dino_runner/components/obstaculs/obstacle.py
+++ dino_runner/components/obstaculs/obstacle.py
@@ -6,9 +6,10 @@ from pygame.sprite import Sprite
 class Obstacle(Sprite):
     Initial_step = 0
     Max_step = 10
-    def __init__(self, images ,index, x_pos = SCREEN_WIDTH):
+    def __init__(self, images ,index, moveto = False, x_pos = SCREEN_WIDTH):
         self.images = images
         self.image = images[index]
+        self.moveto = moveto
         self.image_rect = self.image.get_rect()
         self.image_rect.x = x_pos
         self.step = self.Initial_step
@@ -17,9 +18,10 @@ class Obstacle(Sprite):
         self.image_rect.x -= speed
 
     def draw(self, screen):
-        if self.step > self.Max_step:
-            self.step = self.Initial_step
-        index = 0 if self.step <= 5 else 1
-        self.image = self.images[index]
-        self.step += 1
+        if(self.moveto == True):
+            if self.step > self.Max_step:
+                self.step = self.Initial_step
+            index = 0 if self.step <= 5 else 1
+            self.image = self.images[index]
+            self.step += 1
         screen.blit(self.image, self.image_rect)
