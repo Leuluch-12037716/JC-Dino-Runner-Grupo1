e91701caad43b788ef6a2cfaa963833e967e059e
Leuluch-12037716 <juan32choque@gmail.com>
2022-11-04 11:53:58 +0100

    Update dinosaur.py

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -2,7 +2,7 @@ import pygame
 
 from urllib.parse import SplitResult
 
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 from pygame.sprite import Sprite
 
@@ -25,6 +25,7 @@ class Dinosaur(Sprite):
         self.step = self.Initial_step 
         self.dino_jump = False
         self.dino_run = True
+        self.dino_bend = False
         self.dino_velocity = self.Initial_velocity
 
 
@@ -33,14 +34,24 @@ class Dinosaur(Sprite):
             self.jump()
         if self.dino_run:
             self.run()
+        if self.dino_bend:
+            self.bend()    
 
         if dino_event[pygame.K_SPACE] and not self.dino_jump:
             self.dino_run = False
+            self.dino_bend = False
             self.dino_jump = True
-        #else:
-         #   self.dino_run = True
-          #  self.dino_jump = False
-            
+
+        if dino_event[pygame.K_DOWN] and not self.dino_bend:
+            self.dino_run = False
+            self.dino_jump = False
+            self.dino_bend = True
+
+        if dino_event[pygame.K_UP] and not self.dino_run:
+            self.dino_jump = False
+            self.dino_bend = False
+            self.dino_run = True
+             
 
         if self.step > self.Max_step:    
             self.step = self.Initial_step
@@ -66,4 +77,13 @@ class Dinosaur(Sprite):
 
     def draw(self, screen):
         screen.blit(self.image, (self.image_rect.x, self.image_rect.y))
+
+    def bend(self):
+        self.image = DUCKING[0] if self.step <= 5 else DUCKING[1]
+        self.image_rect = self.image.get_rect()
+        self.image_rect.x = self.Dino_x_pos
+        self.image_rect.y = self.Dino_y_pos + 32
+        self.step += 1
+                
+
             
