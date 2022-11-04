19c8fb7cf8e7429fd2593a153293b11b525dc67f
Leuluch-12037716 <juan32choque@gmail.com>
2022-11-04 01:14:24 +0100

    Update game.py

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -4,7 +4,7 @@ from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, T
 
 from dino_runner.components.dinosaur import Dinosaur
 
-from dino_runner.components.obstaculs.obstacle_handler import Obstaclehandler
+#from dino_runner.components.obstaculs.obstacle_handler import Obstaclehandler
 
 from dino_runner.utils import text_utils
 
@@ -18,7 +18,7 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.dinosaur = Dinosaur()
-        self.obstacle_handler = Obstaclehandler()
+        #self.obstacle_handler = Obstaclehandler()
         self.playing = False
         self.game_speed = 20
         self.x_pos_bg = 0
@@ -43,8 +43,8 @@ class Game:
     def update(self):
         dino_event = pygame.key.get_pressed()
         self.dinosaur.update(dino_event)
-        self.obstacle_handler.update(self.game_speed, self.dinosaur, self.lives)
-        self.update_score
+        #self.obstacle_handler.update(self)
+        #self.update_score()
 
         if self.lives == 0:
             self.playing = False
@@ -54,8 +54,8 @@ class Game:
         self.screen.fill((255, 255, 255))
         self.draw_background()
         self.dinosaur.draw(self.screen)
-        self.obstacle_handler.draw(self.screen)
-        self.draw_score()
+        #self.obstacle_handler.draw(self.screen)
+        #self.draw_score()
         pygame.display.update()
         pygame.display.flip()
 
@@ -68,15 +68,14 @@ class Game:
              self.x_pos_bg = 0
          self.x_pos_bg -= self.game_speed
 
-    def draw_score(self):
-        
-        message = "Points: " + str(self.points)
-        points_text, points_rect = text_utils.get_text_element(message, SCREEN_WIDTH - 100, 70 )
-        self.screen.blit(points_text, points_rect)
+    # def draw_score(self):
+    #     self.points += 1
+    #     if self.points % 100 == 0: 
+    #         self.game_speed += 1
+    #     message = "Points: " + str(self.points)
+    #     points_text, points_rect = text_utils.get_text_element(message, SCREEN_WIDTH - 100, 70 )
+    #     self.screen.blit(points_text, points_rect)
 
-    def update_score(self):
-        self.points += 1
-        if self.points % 100 == 0: 
-            self.game_speed += 1
-
+    # def update_score(self):
+    #     pass
         
