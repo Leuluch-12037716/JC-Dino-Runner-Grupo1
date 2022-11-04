e4cd9b565fccfd68c622a9be98004234e172d4a0
Leuluch-12037716 <juan32choque@gmail.com>
2022-11-04 13:06:43 +0100

    Update bird.py

--- dino_runner/components/obstaculs/bird.py
+++ dino_runner/components/obstaculs/bird.py
@@ -5,5 +5,5 @@ class Bird(Obstacle):
 
     def __init__(self, images):
         index = random.randint(0,1)
-        super().__init__(images, index)
+        super().__init__(images, index, True)
         self.image_rect.y = 320
