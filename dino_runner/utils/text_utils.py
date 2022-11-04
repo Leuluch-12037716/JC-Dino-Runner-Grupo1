a418594af98032bd5be9d6b72ec0de0f0924b4be
Leuluch-12037716 <juan32choque@gmail.com>
2022-11-04 11:20:27 +0100

    Update text_utils.py

--- dino_runner/utils/text_utils.py
+++ dino_runner/utils/text_utils.py
@@ -4,10 +4,10 @@ import pygame
 FONT_STYLE = "freesansbold.ttf"
 BLACK_RGB = (0, 0, 0)
 
-def get_text_element(messege, pos_x = SCREEN_WIDTH //2, pos_y = SCREEN_HEIGHT //2, font_size = 30):
+def get_text_element(messege, pos_x = SCREEN_WIDTH //2, pos_y = SCREEN_HEIGHT //2, font_size = 30, font_color = BLACK_RGB):
     font = pygame.font.Font(FONT_STYLE, font_size)
 
-    text = font.render(messege, True, BLACK_RGB)
+    text = font.render(messege, True, font_color)
     text_rect = text.get_rect()
     text_rect.center = (pos_x, pos_y)
 
