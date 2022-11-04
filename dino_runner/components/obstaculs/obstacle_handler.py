8dd44bfae81a59a460d7e162c00c24dce3327089
Leuluch-12037716 <juan32choque@gmail.com>
2022-11-04 01:30:27 +0100

    Update obstacle_handler.py

--- dino_runner/components/obstaculs/obstacle_handler.py
+++ dino_runner/components/obstaculs/obstacle_handler.py
@@ -7,16 +7,15 @@ class Obstaclehandler():
     def __init__(self):
         self.obstacles = []
     
-    def update(self, game): 
+    def update(self, speed, dino): 
         if len(self.obstacles) == 0:
             self.obstacles.append(Cactus(SMALL_CACTUS))
-            
         for obstacle in self.obstacles:
-            obstacle.update(game.game_speed)
-            if game.dinosaur.image_rect.colliderect(obstacle.image_rect):
+            obstacle.update(speed)
+            if dino.image_rect.colliderect(obstacle.image_rect):
                 pygame.time.delay(300)
                 self.obstacles.pop()
-                game.lives -= 1 
+
 
             if obstacle.image_rect.x < -obstacle.image_rect.width:
                 self.obstacles.pop()
